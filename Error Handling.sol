c68d73d246953ace4aa280c4417e73c4e00a46b2
Swayam Duhan <130980803+swayamduhan@users.noreply.github.com>
2023-12-02 22:46:21 +0530

    Update Error Handling.sol

--- Error Handling.sol
+++ Error Handling.sol
@@ -1,43 +1,20 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
-// Metacrafters Solidity-Intermediate Module 1 
-// Contract should use require(), assert() and revert()
+contract checkingError {
+    function testRequire(uint _num) public pure {
+        require(_num > 10 && _num < 500, "Input must be greater than 10 and less than 500");
+    }
 
-contract ErrorHandling {
-    string public cong = "Congratulations! Value successfully entered";
+    function testRevert(uint _num) public pure {
+        if (_num <= 10) {
+            revert("Input must be greater than 10");
+        }
+    }
+
     uint public num;
 
-//Function to tell the user what to enter as value
-    function valueRequirements(string memory) public pure returns (string memory) {
-        return "Enter a value between 1 to 100 excluding 17 in setValue.";
+    function testAssert() public view {
+        assert(num == 0);
     }
-
-// Function to enter a value between 1 to 100 excluding 17
-    function setValue(uint x) public returns (string memory){
-
-        // reverts the function if 17 is entered
-        if (x == 17) {
-            revert ("17 is not allowed");
-        }
-
-        // a value between 1 to 100 should be added
-        require(x > 0 && x <= 100, "Enter number between 1 and 100");
-
-        //to check any interal error which led to unassignment of value to variable
-        assert(x != 0);
-        
-        num = x;
-
-       return cong;
-
-    }
-
-
-// Function to call the value set by user
-    function getValue() public view returns(uint) {
-        return num;
-    }
-
-      
 }
